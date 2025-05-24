1746c53b89e98bde45e34486e3e8dd39112e62bc
ADMIN <ADMIN@MSI>
2025-05-24 22:57:53 +0700

    Add project files.

---
+++ Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using ToyStore.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CartController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public CartController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpPost("add")]
+    public IActionResult AddToCart([FromBody] CartItemDto item)
+    {
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(_configuration.GetConnectionString("WebCodeDb")))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("add_to_cart", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@user_id", item.UserId);
+                    cmd.Parameters.AddWithValue("@product_id", item.ProductId);
+                    cmd.Parameters.AddWithValue("@product_name", item.ProductName);
+                    cmd.Parameters.AddWithValue("@quantity", item.Quantity);
+                    cmd.Parameters.AddWithValue("@price", item.Price);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return Ok(new { success = true, message = "Đã thêm vào giỏ hàng" });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Lỗi: " + ex.Message);
+        }
+
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToyStore.Models;
+namespace ToyStore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/product
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            try
+            {
+                var count = await _context.Products.CountAsync();
+
+                var products = await _context.Products
+                    .Select(p => new {
+                        p.ProductId,
+                        p.Name,
+                        p.Price1,
+                        p.UrlImage1
+                    })
+                    .ToListAsync();
+
+                return Ok(new { count, data = products });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("💥 Lỗi từ API:");
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Lỗi API: " + ex.Message);
+            }
+        }
+
+
+
+
+    }
+}
